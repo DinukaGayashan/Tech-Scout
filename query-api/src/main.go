1b025eef594dc0f2b046f113bfa78879b226e292
Dinuka <dinukagayashankasthuriarachchi@gmail.com>
2024-06-06 13:21:15 +0530

    file seperation for querying

--- query-api/src/main.go
+++ query-api/src/main.go
@@ -73,45 +73,6 @@ func registerOnDiscovery() {
 	client.SendHeartbeat(instance.App, instance.HostName)
 }
 
-func itemsByQuerying(category string, itemRequestBody ItemRequestBody) ([]Item, error) {
-	query := "SELECT * FROM items WHERE category = ?"
-	args := []interface{}{}
-	args = append(args, category)
-
-	if itemRequestBody.Name != "" {
-		parts := strings.Split(itemRequestBody.Name, " ")
-		for _, j := range parts {
-			query += " AND name LIKE ?"
-			args = append(args, "%"+j+"%")
-		}
-	}
-	if itemRequestBody.Specs != "" {
-		parts := strings.Split(itemRequestBody.Specs, " ")
-		for _, j := range parts {
-			query += " AND specs LIKE ?"
-			args = append(args, "%"+j+"%")
-		}
-	}
-	if itemRequestBody.Shops != "" {
-		parts := strings.Split(itemRequestBody.Shops, " ")
-		for _, j := range parts {
-			query += " AND shops LIKE ?"
-			args = append(args, "%"+j+"%")
-		}
-	}
-
-	rows, _ := DB.Query(query, args...)
-	defer rows.Close()
-
-	var items []Item
-	for rows.Next() {
-		var item Item
-		rows.Scan(&item.ID, &item.Category, &item.Name, &item.Specs, &item.Shops)
-		items = append(items, item)
-	}
-	return items, nil
-}
-
 func getItems(c *gin.Context) {
 	endpoint := c.Request.URL.Path
 	parts := strings.Split(endpoint, "/")
